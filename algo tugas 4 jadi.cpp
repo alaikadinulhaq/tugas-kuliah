1aad41289eb6d976e4e2c67aa1006726dc210ce7
alaikadinulhaq <117826014+alaikadinulhaq@users.noreply.github.com>
2022-11-20 14:22:31 +0700

    Add files via upload

---
+++ algo tugas 4 jadi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	
+	cout<<"\nmasukan bilangan = ";cin>>a;
+	if (a%2==0)
+	cout<<"angka yang anda masukan adalah bilangan genap";
+	else
+	cout<<"angka yang anda masukan adalah bilangan ganjil";
+}
